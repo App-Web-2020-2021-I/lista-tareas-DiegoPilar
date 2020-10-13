a2bf0885b80d6ba7813cf25bea1da2654ccbc17a
Pilucas96 <diego_pilcar@hotmail.com>
2020-10-13 15:29:44 -0500

    Terminacion ejericio jquery

--- app.js
+++ app.js
@@ -1,42 +1,86 @@
-function agregarTarea(datos){
-    let html=$("#lista-tareas").html() +
-    `<li class="list-group-item">
-    <div class="row">
-      <div class="col-1 d-flex">
-        <input type="checkbox" class="form-control align-self-center">
-      </div>
-      <div class="col-10 row m-0 p-0">
-        <div class="col-9 h5">
-          ${datos.titulo}
-        </div>
-        <div class="row-3 text-right">
-          <i>${datos.fecha}</i> 
-        </div>
-        <div class="col-12">
-        ${datos.descripcion}
-        </div>
-      </div>
-      <div class="col-1 d-flex">
-        <button class="btn btn-danger align-self-center">&times;</button>
-      </div>
-    </div>
-  </li>`;
+function agregarTarea(datos) {
 
-  $("#lista-tareas").html(html);
+    var html = $("#lista-tareas").html() + 
+    `
+        <li class="list-group-item" id="${datos.id}">
+            <div class="row">
+                <div class="col-1">
+                    <input type="checkbox" 
+                            class="form-control" 
+                            onchange="cambiarEstatus(${datos.id})">
+                </div>
+                <div class="col-10 row">
+                    <div class="col-9 h5">
+                        ${datos.titulo}
+                    </div>
+                    <div class="col-3 text-right">
+                        <i>${datos.fecha}</i>
+                    </div>
+                    <div class="col-12 text-justify">
+                        ${datos.descripcion}
+                    </div>
+                </div>
+                <div class="col-1">
+                    <button class="btn btn-danger" onclick="borraTarea(${datos.id})">&times;</button>
+                </div>
+            </div>
+        </li>
+    ` ;
+    
+    $("#lista-tareas").html(html);
+}
+
+function muestraAlerta( mensaje ) {
+    $("#alerta").text(mensaje);
+    $("#alerta").show(1000);
+}
+
+function borraTarea(id) {
+    $("#"+id).hide(800,function() {
+        $(this).remove();
+    });
+}
+
+function cambiarEstatus(id) {
+    $("#"+id).toggleClass("bg-success");
+
 }
 
 $(document).ready(function(){
-    $("#form-tarea").submit(function(e){
-        e.preventDefault();
+ 
+    $("#alerta").hide(0);
 
-        let titulo=$("#titulo").val();
-        let fecha=$("#fecha").val();
-        let descripcion=$("#descripcion").val();
-        let datos={
-            titulo,
-            fecha,
-            descripcion
-        };
-        agregarTarea(datos);
-    });
+    $("#forma").submit( function(e){
+            e.preventDefault();
+
+            var titulo      = $("#titulo").val();
+            var fecha       = $("#fecha").val();
+            var descripcion = $("descripcion").val();
+
+            if(titulo.trim() ==="")
+            {
+                muestraAlerta("EL titulo no puede estar vacio");
+                return;
+            }
+            if(fecha ==="")
+            {
+                muestraAlerta("La fecha no puede estar vacia");
+                return;
+            }
+
+
+            var datos = {
+                titulo,
+                fecha,
+                descripcion,
+                id:Date.now()
+            };
+
+            agregarTarea(datos);
+
+            $("#titulo").val("");
+            $("#fecha").val("");
+            $("descripcion").val("");
+    } );
+    
 });
